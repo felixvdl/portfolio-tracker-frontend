7bfa5388fdbd7fd1618ef0d2ab553d4038ae7404
Mark Tanca <email": null>
2017-01-09 16:14:47 -0500

    react component updates realtime data every 3 seconds

--- app/assets/javascripts/components/stock_list.jsx
+++ app/assets/javascripts/components/stock_list.jsx
@@ -11,25 +11,36 @@ class StockList extends React.Component{
   }
 
   componentDidMount(){
-    let portfolio = {"name":"Jessie Dopeness","stocks":[{"stock_id":1,"ticker":"FB","company_name":"Facebook, Inc.","shares_owned":10,"original_price":100.1,"original_value":1001.0,"current_price":125.0837,"current_value":1250.837,"current_pnl":249.837},{"stock_id":2,"ticker":"GOOG","company_name":"Alphabet Inc.","shares_owned":50,"original_price":25.45,"original_value":1272.5,"current_price":806.15,"current_value":40307.5,"current_pnl":39035.0}],"portfolio_pnl":39284.837}
-    this.setState({
-      user: portfolio["name"],
-      portfolioStocks: portfolio["stocks"],
-      pnl: portfolio["portfolio_pnl"],
-      portfolioPnl: portfolio["portfolio_pnl"],
-    })
-    this.newData = setInterval(this.getData,5000)
+var that = this
+    $.ajax({
+        url: 'https://portfolio-tracker-backend.herokuapp.com/users/1/portfolio',
+        dataType: "json",
+      }).done(function(data){
+        that.setState({
+          user: data["name"],
+          portfolioStocks: data["stocks"],
+          pnl: data["portfolio_pnl"],
+          portfolioPnl: data["portfolio_pnl"],
+        })
+      })
+      this.newData = setInterval(this.getData, 3000)
   }
 
   getData(){
-    console.log("sent request")
-    let portfolio = {"name":"Jessie Dopeness","stocks":[{"stock_id":1,"ticker":"FB","company_name":"Facebook, Inc.","shares_owned":10,"original_price":100.1,"original_value":1001.0,"current_price":127.0837,"current_value":1350.837,"current_pnl":249.837},{"stock_id":2,"ticker":"GOOG","company_name":"Alphabet Inc.","shares_owned":50,"original_price":25.45,"original_value":1272.5,"current_price":807.15,"current_value":40307.5,"current_pnl":39035.0}],"portfolio_pnl":40000.837}
-    this.setState({
-      user: portfolio["name"],
-      portfolioStocks: portfolio["stocks"],
-      pnl: portfolio["portfolio_pnl"],
-      portfolioPnl: portfolio["portfolio_pnl"],
-    })
+    console.log("request sent")
+    var that = this
+        $.ajax({
+            url: 'https://portfolio-tracker-backend.herokuapp.com/users/1/portfolio',
+            dataType: "json",
+          }).done(function(data){
+                console.log("request recieved")
+            that.setState({
+              user: data["name"],
+              portfolioStocks: data["stocks"],
+              pnl: data["portfolio_pnl"],
+              portfolioPnl: data["portfolio_pnl"],
+            })
+          })
   }
   render(){
     return(
