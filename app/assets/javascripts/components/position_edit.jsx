0a59d84aa663e7afdfb0de1e09aa4fc4a980f6f4
Mark Tanca <email": null>
2017-01-10 11:45:25 -0500

    added initial stock profile data to stock profile data page

--- app/assets/javascripts/components/position_edit.jsx
+++ app/assets/javascripts/components/position_edit.jsx
@@ -1,7 +1,9 @@
 class PositionEdit extends React.Component{
   render(){
     return(
-        <form action="https://portfolio-tracker-backend.herokuapp.com/users/1/stocks/1" method="get">
+      <div>
+        <h3> Make a Trade: </h3>
+        <form action="https://portfolio-tracker-backend.herokuapp.com/users/1/stocks/1" method="post">
           <select id="position-type">
             <option value="buy" >Buy</option>
             <option value="sell" >Sell</option>
@@ -10,6 +12,7 @@ class PositionEdit extends React.Component{
          <input type="text" name="share_amount"/>
          <input id="position-sumbit-button" type="submit" value="Update Position"/>
        </form>
+     </div>
 
     )
   }
