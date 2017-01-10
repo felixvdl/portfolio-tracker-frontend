0a2698e315b2c376235773b0420e84b3128dcc62
Felix Vandelanotte <vandelanottefelix@gmail.com>
2017-01-10 17:56:20 -0500

    finish charts

--- app/assets/javascripts/components/year_line_chart.jsx
+++ app/assets/javascripts/components/year_line_chart.jsx
@@ -1,17 +1,17 @@
-class MonthlyLine extends React.Component {
+class YearlyLine extends React.Component {
   componentDidMount(){
-    let month = [];
-    for (var i = 0; i < 31; i++) {
-      datum = new Date(Date.now() - (i * 86400000)),
-      month = month.concat([datum.toDateString()])
+    let week = [];
+    for (var i = 0; i < 12; i++) {
+      datum = new Date(Date.now() - ((i * 31)* 86400000)),
+      week = week.concat([datum.toDateString()])
     }
     let data = {
 
-    labels: [month[30], month[29], month[28], month[27], month[26], month[25], month[24], month[23], month[22], month[21], month[20], month[19], month[18], month[17], month[16], month[15], month[14], month[13], month[12], month[11], month[10], month[9], month[8], month[7], month[6], month[5], month[4], month[3], month[2], month[1], month[0]],
+    labels:  [week[11].slice(4,7), week[10].slice(4,7), week[9].slice(4,7), week[8].slice(4,7), week[7].slice(4,7), week[6].slice(4,7), week[5].slice(4,7), week[4].slice(4,7), week[3].slice(4,7), week[2].slice(4,7), week[1].slice(4,7), week[0].slice(4,7)],
     datasets: [
         {
             label: "P&L",
-            fill: false,
+            fill: true,
             lineTension: 0.1,
             backgroundColor: "rgba(75,192,192,0.4)",
             borderColor: "rgba(75,192,192,1)",
@@ -33,7 +33,7 @@ class MonthlyLine extends React.Component {
         },
         {
             label: "%",
-            fill: false,
+            fill: true,
             lineTension: 0.1,
             backgroundColor: "rgba(75,192,192,0.4)",
             borderColor: "rgba(75,192,192,1)",
